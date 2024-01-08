6f721b488135660224e19557fa381085c7191063
Hanane <hanane.elkaaba1@gmail.com>
2024-01-08 18:23:14 +0100

    Add audios

--- pages/[searchName].jsx
+++ pages/[searchName].jsx
@@ -1,7 +1,9 @@
 import NavBar from "@/components/NavBar";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function SearchName({ word }) {
+  const [audioPlaying, setAudioPlaying] = useState(false);
   const nounDefinitions = [];
   const verbDefinitions = [];
   word.forEach((entry) => {
@@ -17,45 +19,80 @@ export default function SearchName({ word }) {
       }
     });
   });
+
+  const handlePlay = () => {
+    const audioElement = new Audio(
+      word[0].phonetics[1].audio
+        ? word[0].phonetics[1].audio
+        : word[0].phonetics[0].audio
+    );
+
+    audioElement.addEventListener("playing", () => setAudioPlaying(true));
+    audioElement.addEventListener("ended", () => setAudioPlaying(false));
+
+    audioElement.play();
+  };
+
   return (
-    <div className="px-6">
+    <div className="px-6 pb-10">
       <NavBar />
       <div className="flex justify-between shrink-0 items-center">
         <div className="my-5">
           <p className="text-midnight-black font-bold text-2xl ">
             {word[0].word}
           </p>
-          <span className="text-royal-purple">{word[0].phonetics[1].text}</span>
+          <span className="text-royal-purple">
+            {word[0].phonetic || word[0].phonetics[1].text}
+          </span>
         </div>
-        <button className="bg-pale-purple rounded-full p-3.5">
+
+        <button
+          className="bg-pale-purple rounded-full p-3.5"
+          onClick={handlePlay}
+        >
           <Image src="./play.svg" alt="play button" width="20" height="20" />
         </button>
       </div>
 
-      <div class="flex items-center">
-        <p class="mr-1.5 font-bold text-midnight-black text-base">noun</p>
-        <div class="border-t border-medium-gray flex-1"></div>
-      </div>
+      {nounDefinitions.length > 0 && (
+        <div>
+          <div class="flex items-center">
+            <p class="mr-1.5 font-bold text-midnight-black text-base">noun</p>
+            <div class="border-t border-medium-gray flex-1"></div>
+          </div>
 
-      <section>
-        <p className="my-5 text-medium-gray">Meaning</p>
-        <ul className="list-disc ml-5 marker:text-royal-purple text-sm">
-          {nounDefinitions.map((definition, index) => (
-            <li key={index} className="mb-3.5">
-              {definition}
-            </li>
-          ))}
-        </ul>
-      </section>
+          <section>
+            <p className="my-5 text-medium-gray">Meaning</p>
+            <ul className="list-disc ml-5 marker:text-royal-purple text-sm">
+              {nounDefinitions.map((definition, index) => (
+                <li key={index} className="mb-3.5">
+                  {definition}
+                </li>
+              ))}
+            </ul>
+          </section>
+        </div>
+      )}
 
-      <p>verb</p>
-      <p>Meaning</p>
-      <ul>
-        {verbDefinitions.map((definition, index) => (
-          <li key={index}>{definition}</li>
-        ))}
-      </ul>
-      <a href={word.sourceUrls}>{word.sourceUrls}</a>
+      {verbDefinitions.length > 0 && (
+        <div>
+          <div class="flex items-center">
+            <p class="mr-1.5 font-bold text-midnight-black text-base">verb</p>
+            <div class="border-t border-medium-gray flex-1"></div>
+          </div>
+
+          <section>
+            <p className="my-5 text-medium-gray">Meaning</p>
+            <ul className="list-disc ml-5 marker:text-royal-purple text-sm">
+              {verbDefinitions.map((definition, index) => (
+                <li key={index} className="mb-3.5">
+                  {definition}
+                </li>
+              ))}
+            </ul>
+          </section>
+        </div>
+      )}
     </div>
   );
 }
