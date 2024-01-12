c11aa55b3112946c22e3d18d5e719e4420e8ab64
Hanane <hanane.elkaaba1@gmail.com>
2024-01-12 12:45:19 +0100

    Modify the manner in which the information is presented.

--- pages/[searchName].jsx
+++ pages/[searchName].jsx
@@ -1,27 +1,20 @@
 import NavBar from "@/components/NavBar";
 import Image from "next/image";
+import { Fragment } from "react";
 import { useState } from "react";
 
 export default function SearchName({ word }) {
   const [audioPlaying, setAudioPlaying] = useState(false);
-  const nounDefinitions = [];
-  const verbDefinitions = [];
-  word.forEach((entry) => {
-    entry.meanings.forEach((meaning) => {
-      if (meaning.definitions) {
-        meaning.definitions.forEach((definition) => {
-          if (meaning.partOfSpeech === "noun") {
-            nounDefinitions.push(definition.definition);
-          } else if (meaning.partOfSpeech === "verb") {
-            verbDefinitions.push(definition.definition);
-          }
-        });
-      }
-    });
-  });
 
   const handlePlay = () => {
-    const audioElement = new Audio(word[0].phonetics[1].audio);
+    let audioUrl;
+    word[0].phonetics.forEach((phonetic) => {
+      if (phonetic.audio.length > 0) {
+        audioUrl = phonetic.audio;
+      }
+    });
+
+    const audioElement = new Audio(audioUrl);
 
     audioElement.addEventListener("playing", () => setAudioPlaying(true));
     audioElement.addEventListener("ended", () => setAudioPlaying(false));
@@ -32,6 +25,7 @@ export default function SearchName({ word }) {
   return (
     <div className="px-6 pb-10 dark:bg-midnight-black">
       <NavBar />
+
       <div className="flex justify-between shrink-0 items-center">
         <div className="my-5">
           <p className="text-midnight-black font-bold text-2xl dark:text-white">
@@ -50,49 +44,42 @@ export default function SearchName({ word }) {
         </button>
       </div>
 
-      {nounDefinitions.length > 0 && (
-        <div>
-          <div className="flex items-center">
-            <p className="mr-1.5 font-bold text-midnight-black text-base dark:text-white">
-              noun
+      {word[0].meanings.map((meaning, index) => (
+        <ul key={index}>
+          <p className="mb-3 mt-5 text-medium-gray">Meaning</p>
+          <li className="flex items-center">
+            <p className="mr-1.5 font-bold text-midnight-black text-base mb-3.5 dark:text-white">
+              {meaning.partOfSpeech}
             </p>
             <div className="border-t border-medium-gray flex-1"></div>
-          </div>
-
-          <section>
-            <p className="my-5 text-medium-gray">Meaning</p>
-            <ul className="list-disc ml-5 marker:text-royal-purple text-sm">
-              {nounDefinitions.map((definition, index) => (
-                <li key={index} className="mb-3.5">
-                  {definition}
-                </li>
+          </li>
+          <li>
+            <ul className="list-disc marker:text-royal-purple pl-5 text-sm">
+              {meaning.definitions.map((definition, index) => (
+                <Fragment key={index}>
+                  <li className="mb-3.5">{definition.definition}</li>
+                  {definition.example && (
+                    <li className="text-medium-gray list-none mb-3.5">
+                      &quot;{definition.example}&quot;
+                    </li>
+                  )}
+                </Fragment>
               ))}
             </ul>
-          </section>
-        </div>
-      )}
 
-      {verbDefinitions.length > 0 && (
-        <div>
-          <div className="flex items-center">
-            <p className="mr-1.5 font-bold text-midnight-black text-base dark:text-white">
-              verb
-            </p>
-            <div className="border-t border-medium-gray flex-1"></div>
-          </div>
-
-          <section>
-            <p className="my-5 text-medium-gray">Meaning</p>
-            <ul className="list-disc ml-5 marker:text-royal-purple text-sm">
-              {verbDefinitions.map((definition, index) => (
-                <li key={index} className="mb-3.5">
-                  {definition}
-                </li>
-              ))}
+            <ul className="flex text-sm flex-wrap">
+              <p className="mr-4">Synonyms</p>
+              {meaning.synonyms.map((synonym, index) => {
+                return (
+                  <li key={index} className="text-royal-purple mr-4">
+                    {synonym}
+                  </li>
+                );
+              })}
             </ul>
-          </section>
-        </div>
-      )}
+          </li>
+        </ul>
+      ))}
     </div>
   );
 }
@@ -104,7 +91,7 @@ export async function getServerSideProps(context) {
       `https://api.dictionaryapi.dev/api/v2/entries/en/${searchName}`
     );
     const data = await response.json();
-    console.log(data);
+
     return {
       props: {
         word: data,
